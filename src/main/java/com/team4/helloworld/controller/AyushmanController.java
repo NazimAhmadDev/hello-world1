2163cbbfef76e6199244c44317a9b33e0c670d39
Ayushman351 <ayushmandixit4@gmail.com>
2025-06-05 15:33:59 +0530

    [ayushman]:adding AyushmanController.java

---
+++ src/main/java/com/team4/helloworld/controller/AyushmanController.java
@@ -0,0 +1,15 @@
+package com.team4.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ayushman")
+public class AyushmanController {
+
+    @GetMapping("/get/hello")
+    public String getHello(){
+        return "Hello, from Ayushman";
+    }
+}
